851cf61264a1653affa38f745b9ca25135ec2434
QueenMae <maena.brice@epitech.digital>
2024-10-08 13:32:09 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,17 @@
+let currentRotation = 0;
+function rotateCircle(step) {
+    const circle = document.getElementById('circle');
+      currentRotation += step;
+ circle.style.transform = `rotate(${currentRotation}deg)`;
+}
+document.querySelectorAll('.rotate-button').forEach(button =>
+    button.addEventListener('click', () => rotateCircle(parseInt(button.value)))
+);
+
+
+let reset = document.getElementById("rotate-reset")
+reset.addEventListener('click', () => {
+
+    currentRotation = 0;
+    circle.style.transform = `rotate(0 deg)`
+})
